0c3216d7d3041c682531c2ecfbfa0314c929c808
SibusisoMabuya <sbubass@gmail.com>
2026-09-19 02:20:21 +0200

    Everything thus far

---
+++ app.js
@@ -0,0 +1,18 @@
+// const inputs = document.querySelectorAll(".input");
+
+// function focusFunc() {
+//   let parent = this.pareantNode;
+//   parent.classList.add("focus");
+// }
+
+// function blurFunc() {
+//   let parent = this.pareantNode;
+//   if (this.value == "") {
+//     parent.classList.remove("focus");
+//   }
+// }
+
+// inputs.forEach((input) => {
+//   input.addEventListener("focus", focusFunc);
+//   input.addEventListener("blur", blurFunc);
+// });
